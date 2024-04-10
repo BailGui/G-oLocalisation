1ee0a8063529bbf388007469855f088d6211c5da
Guillaume <guillaumebaillieux88@gmail.com>
2024-04-10 10:47:14 +0200

    exmarker

---
+++ excluster/cluster.js
@@ -0,0 +1,17 @@
+/* Définition du centre de la carte et du zoom (valeur initiale) */
+const carte = L.map('map').setView([51.505, -0.09], 13);
+
+/* ajout d'un background */
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(carte);
+
+/* Ajout d'un liste de point à placer sur la carte */
+const liste = {
+    "Studios Abbey Road": {"lat":54.531988,"lng":-0.178226},
+    "Palais de Buckingham": {"lat":51.500835, "lng":-0.143004},
+    "Trafalgar Square": {"lat":51.508037,"lng":-0.128049},
+    "British Museum":{"lat":51.519294,"lng":-0.128018}
+};
+
