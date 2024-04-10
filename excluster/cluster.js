fa87fd4b0d8e81f45d9080f7a23ccc430665c5e4
Guillaume <guillaumebaillieux88@gmail.com>
2024-04-10 11:21:13 +0200

    excluster

--- excluster/cluster.js
+++ excluster/cluster.js
@@ -9,7 +9,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 /* Ajout d'un liste de point à placer sur la carte */
 const liste = {
-    "Studios Abbey Road": {"lat":54.531988,"lng":-0.178226},
+    "Studios Abbey Road": {"lat":51.531988,"lng":-0.178226},
     "Palais de Buckingham": {"lat":51.500835, "lng":-0.143004},
     "Trafalgar Square": {"lat":51.508037,"lng":-0.128049},
     "British Museum":{"lat":51.519294,"lng":-0.128018}
@@ -17,6 +17,9 @@ const liste = {
 
 /* Création d'un ClusterGroup */
 const markers= L.markerClusterGroup();
+
+/*Création d'un tableau de marqueurs pour un affichage optimal avec FeatureGroup */
+const markerTable = [];
 
 /*Boucle pour créer les marqueurs de la liste */
 for (let item in liste){
@@ -26,7 +29,16 @@ for (let item in liste){
     unMarqueur.bindPopup(item);
     /* ajouter ce marqueur au groupe */
     markers.addLayer(unMarqueur);
+
+    /*ajouter ce marqueur au tableau */
+    markerTable.push(unMarqueur);
 }
 
 /* ajouter le groupe a la carte */
 carte.addLayer(markers);
+
+/* placer le tableau de marqueurs dans le featureGroup */
+const groupe = new L.featureGroup(markerTable);
+
+/* adapter l'affichage de ma carte en fonction de la position des marqueurs */
+carte.fitBounds(groupe.getBounds());
