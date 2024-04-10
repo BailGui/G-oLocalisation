be429b14a818f9d4b1a1f424e6746346a5edfe2f
Guillaume <guillaumebaillieux88@gmail.com>
2024-04-10 09:42:21 +0200

    exmarker

--- exmarker/exmarker.js
+++ exmarker/exmarker.js
@@ -1,1 +1,8 @@
-var carte = L.map('map').setView([51.505, -0.09], 13);
+/* Définition du centre de la carte et du zoom (valeur initiale) */
+const carte = L.map('map').setView([51.505, -0.09], 13);
+
+/* ajout d'un background */
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(carte);
