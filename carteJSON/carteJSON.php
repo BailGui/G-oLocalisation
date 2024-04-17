6fbb0faf842f294d2b347d76fffd6469ba72f648
Guillaume <guillaumebaillieux88@gmail.com>
2024-04-17 13:39:34 +0200

    majTryCatch

---
+++ carteJSON/carteJSON.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once "config.php";
+
+
+
+try {
+    $db = new PDO(DB_DRIVER . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET . ";port=" . DB_PORT, DB_LOGIN, DB_PWD);
+} catch (Exception $e) {
+    die($e->getMessage());
+}
+
+$db = null;
+
+function getLocations(PDO $db): array
+{
+    $sql = "SELECT * FROM locations ORDER BY id ASC";
+    $query = $db->query($sql);
+    $result = $query->fetchAll(PDO::FETCH_ASSOC);
+    $query->closeCursor();
+    return $result;
+
+}
