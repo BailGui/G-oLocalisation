c9265ee258e504618d43a6eee42db1cdf8276f91
Guillaume <guillaumebaillieux88@gmail.com>
2024-04-17 13:32:56 +0200

    majConfig

---
+++ carteJSON/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// constantes de connexions
+const DB_DRIVER = "mysql";
+const DB_HOST = "localhost";
+const DB_PORT = 3306;
+const DB_CHARSET = "utf8mb4";
+const DB_NAME = "belgium_locations";
+const DB_PWD = "";
+const DB_LOGIN = "root";
